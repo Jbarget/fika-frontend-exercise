bb71bb2fc027f121fbfe8b234e7aed7f0d037f88
jbarget <dear.justen@gmail.com>
2022-03-09 20:59:17 +0100

    add SearchBar component & create theme/themeHelpers

---
+++ theme.ts
@@ -0,0 +1,21 @@
+export type Color = keyof typeof colors;
+export const colors = {
+  richBlack: '#073B3A',
+  darkLiver: '#554348',
+  pewterBlue: '#93B7BE',
+  lightCyan: '#D4F5F5',
+  cadetGrey: '#8C9A9E',
+  silverSand: '#BDC5C7',
+};
+
+export type Space = keyof typeof space;
+export const space = {
+  0: 0,
+  1: 4,
+  2: 8,
+  3: 12,
+  4: 16,
+  5: 20,
+  6: 24,
+  7: 28,
+};
